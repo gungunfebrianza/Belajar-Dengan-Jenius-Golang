c5c4cae0b13e875422a7b903b773f74e321c3e1c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-05 21:49:28 +0700

    Hello World

---
+++ src/Intro/Hello/hello.go
@@ -0,0 +1,15 @@
+//the rest of the code belongs to main package
+package main
+
+/*
+We will using text-formatting code
+from the fmt package
+*/
+import (
+	"fmt"
+)
+
+//entrypoint
+func main() {
+	fmt.Println("Hello World!")
+}
