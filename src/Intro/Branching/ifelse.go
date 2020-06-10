e83f6ab755865bc75cc864cff3576988b309c148
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-11 02:29:45 +0700

    AddBranching

---
+++ src/Intro/Branching/ifelse.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	input := 9
+
+	if input < 0 {
+		fmt.Println("Input Harus Angka Positif")
+	} else if input%2 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+
+}
+
+// Memorizing
+// Typing
+// Metric
