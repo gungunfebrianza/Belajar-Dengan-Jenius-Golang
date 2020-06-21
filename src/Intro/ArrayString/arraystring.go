c747b310f41adb91ed9134428513b0886fbdec4b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-21 22:45:15 +0700

    AddArrayString

---
+++ src/Intro/ArrayString/arraystring.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myArray [3]string
+	myArray[0] = "Maudy"
+	myArray[1] = "Ayunda"
+	myArray[2] = "Faza"
+	fmt.Println(myArray)
+
+	x := [3]float64{2.2, 3.3, 4.4}
+	fmt.Println(x) // [2.2 3.3 4.4]
+
+}
