ac95bd103235a76ab38a989c1e13c7518ec8f13d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-21 22:44:49 +0700

    AddSlice

---
+++ src/Intro/Slice/slice.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mySlice []int
+	mySlice = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(mySlice) //[1 2 3 4 5]
+
+	var slice []string
+	slice = []string{"Bapak", "Sanusi", "Ariyanto"}
+	fmt.Println(slice)
+}
