68f023886e8a00c3475642ba89817dbb8118d598
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-27 20:26:18 +0700

    AddMap

---
+++ src/Intro/Map/map.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var couple = map[int]string{1 :"Maudy Ayunda", 9: "Gun Gun", 11:"Bpk.Ariyanto"}
+	fmt.Println(couple) //map[1:Maudy Ayunda 9:Gun Gun 11:Bpk.Ariyanto]
+}
